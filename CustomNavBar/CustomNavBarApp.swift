aa0b83a3829adfff8157380a1c1edc5c22bce20e
jabdb <hy.l@kakao.com>
2022-03-25 09:44:30 +0900

    Initial Commit

---
+++ CustomNavBar/CustomNavBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavBarApp.swift
+//  CustomNavBar
+//
+//  Created by 이호영 on 2022/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
